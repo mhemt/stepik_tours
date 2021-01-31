4eee4681c09e85c65ba78e9e763090859c0199c0
mhemt <74146735+mhemt@users.noreply.github.com>
2021-01-31 19:00:03 +0300

    Week 2 task complete.

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tours.views import main_view, departure_view, tour_view, custom_handler404, custom_handler500
+from tours.views import MainView, DepartureView, TourView, custom_handler404, custom_handler500
 
 urlpatterns = [
-    path('', main_view),
-    path('departure/<str:departure>/', departure_view),
-    path('tour/<int:id>/', tour_view),
+    path('', MainView.as_view(), name='main'),
+    path('departure/<str:departure>/', DepartureView.as_view(), name='departure'),
+    path('tour/<int:id>/', TourView.as_view(), name='tour'),
+
 ]
 
 handler404 = custom_handler404
--- tours/views.py
+++ tours/views.py
@@ -1,17 +1,94 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound, HttpResponseServerError
+from django.views import View
+from tours import data
+from random import randint
 
 
-def main_view(request):
-    return render(request, 'index.html')
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        tours = {}
+        while len(tours) < 6:
+            tours_len = randint(1, len(data.tours))
+            tours[tours_len] = data.tours[tours_len]
+
+        title = data.title
+        subtitle = data.subtitle
+        description = data.description
+        departures = data.departures
+
+        context = {
+            'title': title,
+            'subtitle': subtitle,
+            'description': description,
+            'departures': departures,
+            'tours': tours,
+        }
+        return render(request, 'index.html', context)
 
 
-def departure_view(request, departure):
-    return render(request, 'departure.html')
+class DepartureView(View):
+    def get(self, request, *args, **kwargs):
+        departure_code = kwargs['departure']
+        title = data.title
+        departures = data.departures
+        tours = {id_: tour for id_, tour in data.tours.items() if tour['departure'] == departure_code}
+        tours_num = len(tours)
+        tours_min_price = min(tours.values(), key=lambda x: x['price'])['price']
+        tours_max_price = max(tours.values(), key=lambda x: x['price'])['price']
+        tours_min_price = str(tours_min_price)[:-3] + ' ' + str(tours_min_price)[-3:]
+        tours_max_price = str(tours_max_price)[:-3] + ' ' + str(tours_max_price)[-3:]
+        tours_min_nights = min(tours.values(), key=lambda x: x['nights'])['nights']
+        tours_max_nights = max(tours.values(), key=lambda x: x['nights'])['nights']
+        departure = departures[departure_code].split()[-1]
+
+        context = {
+            'departure_code': departure_code,
+            'title': title,
+            'departures': departures,
+            'tours': tours,
+            'tours_num': tours_num,
+            'tours_min_price': tours_min_price,
+            'tours_max_price': tours_max_price,
+            'tours_min_nights': tours_min_nights,
+            'tours_max_nights': tours_max_nights,
+            'departure': departure
+        }
+
+        return render(request, 'departure.html', context)
 
 
-def tour_view(request, id):
-    return render(request, 'tour.html')
+class TourView(View):
+    def get(self, request, *args, **kwargs):
+        id_ = kwargs['id']
+        title = data.title
+        departures = data.departures
+        departure_code = data.tours[id_]['departure']
+        tour_title = data.tours[id_]['title']
+        tour_description = data.tours[id_]['description']
+        tour_departure = departures[departure_code].split()[1]
+        tour_picture = data.tours[id_]['picture']
+        tour_price = str(data.tours[id_]['price'])[:-3] + ' ' + str(data.tours[id_]['price'])[-3:]
+        tour_stars = int(data.tours[id_]['stars']) * '★'
+        tour_country = data.tours[id_]['country']
+        tour_nights = data.tours[id_]['nights']
+        tour_date = data.tours[id_]['date']
+
+        context = {
+            'title': title,
+            'departures': departures,
+            'departure_code': departure_code,
+            'tour_title': tour_title,
+            'tour_description': tour_description,
+            'tour_departure': tour_departure,
+            'tour_picture': tour_picture,
+            'tour_price': tour_price,
+            'tour_stars': tour_stars,
+            'tour_country': tour_country,
+            'tour_nights': tour_nights,
+            'tour_date': tour_date,
+        }
+        return render(request, 'tour.html', context)
 
 
 def custom_handler404(request, exception):
